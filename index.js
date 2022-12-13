3f27c07ff6394618f43c38b6ea1300cebdf2cc06
Luana <lugamoreira@gmail.com>
2022-12-12 21:00:45 -0500

    commit tree 6fef4f270010ee74eb8d864e49fff961f19b8558

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function(){
+    return 2000/100
+};
+const square = function(){
+    let x = 2
+    return x*x ;
+}
+const add = function(){
+    let a = 3
+    let b = 4
+    return a+b;
+}
